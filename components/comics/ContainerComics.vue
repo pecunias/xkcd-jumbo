dd0e63dfb2ba3035b17cad5c789118916244ed22
Jelte Wiersma <jelte_wiersma@hotmail.com>
2020-12-03 19:29:12 +0100

    copy repository because of issues
    Signed-off-by: Jelte Wiersma <jelte_wiersma@hotmail.com>

---
+++ components/comics/ContainerComics.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="container">
+    <h1>test</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-family: sans-serif;
+}
+
+h1 {
+  margin-bottom: 20px;
+}
+
+.movie img {
+  width: 300px;
+  height: 445px;
+}
+
+.movie h2 {
+  margin-top: 5px;
+}
+
+button {
+  margin-top: 15px;
+  cursor: pointer;
+}
+</style>
---
+++ components/login/LoginUser.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <form id="app"
+      @submit="login"
+      action="/login"
+      method="POST">
+      <p>
+        <label for="username">Username </label>
+        <input type="text" id="username" required>
+      </p>
+      <p>
+        <label for="password">Password </label>
+        <input type="password" id="password" required>
+      </p>
+      <input type="submit" value="Submit">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginUser',
+  methods: {
+    login: function (e) {
+      e.preventDefault();
+
+      this.$router.push({
+          path: '/comics'
+      });
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
